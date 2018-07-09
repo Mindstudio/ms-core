656abd612d051a9c907d7468d14f54e0a531edc2
KC Wesley <KCWx9@tuta.io>
2018-07-09 17:28:54 +0100

    remove server

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-import adminRoutes from '@/modules/admin/_routes/admin.routes.js'
 import focusRoutes from '@/modules/focus/_routes/focus.routes.js'
 import libraryRoutes from '@/modules/lib/_routes/library.routes.js'
 
@@ -10,7 +9,6 @@ Vue.use(Router)
 const baseRoutes = []
 
 const routes = baseRoutes.concat(
-  adminRoutes,
   focusRoutes,
   libraryRoutes
 )
